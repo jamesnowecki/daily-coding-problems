74ba68f687768432a16d5b3c9674e7d7d7ecd6ad
James Nowecki <jamesnowecki@gmail.com>
2020-07-10 08:53:44 +0100

    basic spiral written, need to figure out for loop numbering

---
+++ 8July2020/main.js
@@ -0,0 +1,46 @@
+// Given a N by M matrix of numbers, print out the matrix in a clockwise spiral.
+
+const theMatrix = [
+    [1,  2,  3,  4,  5],
+    [6,  7,  8,  9,  10],
+    [11, 12, 13, 14, 15],
+    [16, 17, 18, 19, 20]
+];
+
+const printSpiral = (matrix) => {
+    let matrixCopy = [...matrix];
+    const n = matrixCopy[0].length;
+    const m = matrixCopy.length;
+
+    for (let i = 0; i < 5; i++) {
+
+        let topRow = matrixCopy.splice(0,1);
+
+        topRow.forEach(item => {
+            console.log(item)
+        });
+
+        matrixCopy.forEach(item => {
+            const endNumber = item.pop();
+            console.log(endNumber)
+        });
+
+        let bottomRow = matrixCopy.pop().reverse();
+
+        bottomRow.forEach(item => {
+            console.log(item)
+        });
+
+        let leftNumbers = [];
+
+        matrixCopy.forEach(item => {
+            leftNumbers.push(item.splice(0,1))
+        });
+
+        let leftNumbersReversed = leftNumbers.reverse();
+
+        leftNumbersReversed.forEach(item => {
+            console.log(item);
+        })
+      }
+}
